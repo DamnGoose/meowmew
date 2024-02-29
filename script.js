9452f2b63804627d4fde97a89c57b8000ef76abe
Гусяка <74218254+DamnGoose@users.noreply.github.com>
2024-02-29 22:21:42 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const audio = new Audio('meow.mp3');
+
+function playAudio() {
+    audio.play();
+    // Удаляем обработчик события click после первого воспроизведения
+    document.body.removeEventListener('click', playAudio);
+}
+
+const patternText = " МЯУ ";
+const patternWidth = 60;
+const patternHeight = 40;
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+const rowCount = Math.ceil(screenHeight / patternHeight);
+const colCount = Math.ceil(screenWidth / patternWidth);
+
+for (let i = 0; i < rowCount; i++) {
+    for (let j = 0; j < colCount; j++) {
+        const pattern = document.createElement('div');
+        pattern.classList.add('pattern');
+        pattern.textContent = patternText;
+        pattern.style.top = i * patternHeight + 'px';
+        pattern.style.left = j * patternWidth + 'px';
+        document.body.appendChild(pattern);
+    }
+}
